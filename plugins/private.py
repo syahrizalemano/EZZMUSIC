12819129854becb73bf3aaff166712b18b38ba34
syahrizalemano <bitano302@gmail.com>
2022-01-25 10:11:39 +0800

    upload ini

---
+++ plugins/private.py
@@ -0,0 +1,97 @@
+"""
+VideoPlayerBot, Telegram Video Chat Bot
+Copyright (c) 2021  Asm Safone <https://github.com/AsmSafone>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>
+"""
+
+import asyncio
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from pyrogram.errors import MessageNotModified
+from helpers.bot_utils import BOT_NAME, USERNAME
+from config import SUPPORT_GROUP, UPDATES_CHANNEL
+from translations import START_TEXT, HELP_TEXT, ABOUT_TEXT
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
+
+@Client.on_message(filters.command(["start", f"start@{USERNAME}"]))
+async def start(client, message):
+   buttons = [
+            [
+                InlineKeyboardButton("EZZRA", url="https://t.me/ezzraez")
+            ]
+            ]
+   reply_markup = InlineKeyboardMarkup(buttons)
+   if message.chat.type == 'private':
+       await message.reply_text(
+          START_TEXT,
+          reply_markup=reply_markup
+       )
+   else:
+      await message.reply_text(f"**{BOT_NAME} is Alive !** ✨")
+
+@Client.on_callback_query()
+async def cb_handler(client: Client, query: CallbackQuery):
+    if query.data=="help":
+        buttons = [
+            [
+                InlineKeyboardButton("🔙 BACK", callback_data="start"),
+            ]
+            ]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        try:
+            await query.edit_message_text(
+                HELP_TEXT,
+                reply_markup=reply_markup
+            )
+        except MessageNotModified:
+            pass
+
+    elif query.data=="about":
+        buttons = [
+            [
+                InlineKeyboardButton("🔙 BACK", callback_data="start"),
+            ]
+            ]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        try:
+            await query.edit_message_text(
+                ABOUT_TEXT,
+                reply_markup=reply_markup
+            )
+        except MessageNotModified:
+            pass
+
+    elif query.data=="about":
+        buttons = [
+            [
+                InlineKeyboardButton("🔙 BACK", callback_data="start"),
+            ]
+            ]
+        reply_markup = InlineKeyboardMarkup(buttons)
+        try:
+            await query.edit_message_text(
+                START_TEXT,
+                reply_markup=reply_markup
+            )
+        except MessageNotModified:
+            pass
+
+    elif query.data=="close":
+        try:
+            await query.message.delete()
+            await query.message.reply_to_message.delete()
+        except:
+            pass
+
